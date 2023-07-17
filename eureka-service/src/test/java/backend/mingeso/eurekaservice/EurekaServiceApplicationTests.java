bb85261dc31ba79417373cb976753522aaa95eb3
Daniela <daniela.orellana.c@usach.cl>
2023-07-17 19:26:37 -0400

    creación de microservicio

---
+++ eureka-service/src/test/java/backend/mingeso/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.mingeso.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
